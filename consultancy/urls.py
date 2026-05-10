7603265da028c462330fa258a3a9b79f05ccfbcb
Sulekha Stalin <sulekhastalin@192.168.1.10>
2026-05-10 21:25:12 +0530

    Fix booking url

--- consultancy/urls.py
+++ consultancy/urls.py
@@ -1,9 +1,10 @@
+from django.contrib import admin
 from django.urls import path
-from . import views
+from consultancy import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('booking/', views.booking, name='booking'),
     path('services/', views.services, name='services'),
-
 ]
