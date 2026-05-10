e36b741bd85399db43182c6316e1dc71f12c5ed9
Sulekha Stalin <sulekhastalin@192.168.1.10>
2026-05-10 22:35:39 +0530

    fix email timeout

--- TaxConsultancy/settings.py
+++ TaxConsultancy/settings.py
@@ -134,7 +134,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_TIMEOUT = 60
+EMAIL_TIMEOUT = 5
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
