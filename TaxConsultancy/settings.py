620d176c647db2bc6578e9bdefe07fe6973de6d2
Sulekha Stalin <sulekhastalin@192.168.1.10>
2026-05-10 21:27:57 +0530

    Fix production email settings

--- TaxConsultancy/settings.py
+++ TaxConsultancy/settings.py
@@ -127,19 +127,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'nexgentax2025@gmail.com'
-
-EMAIL_HOST_PASSWORD = 'unlv pems vlxx khxy'
-
-
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
