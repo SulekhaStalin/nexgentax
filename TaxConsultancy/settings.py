e3a288bc89e58fab15732c8e8231741ce22d9dd5
Sulekha Stalin <sulekhastalin@192.168.1.10>
2026-05-10 21:51:41 +0530

    Fix email backend typo

--- TaxConsultancy/settings.py
+++ TaxConsultancy/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 
 
-EEMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
