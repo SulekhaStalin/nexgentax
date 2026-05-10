fe0fbfa8c5968411f9e6d70c2c60bf9d45455558
Sulekha Stalin <sulekhastalin@192.168.1.10>
2026-05-10 22:16:45 +0530

    Fix Brevo SMTP host

--- TaxConsultancy/settings.py
+++ TaxConsultancy/settings.py
@@ -131,11 +131,12 @@ USE_TZ = True
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_TIMEOUT = 20
 
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
